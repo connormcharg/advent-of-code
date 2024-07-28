94c337ebd23789a01072b82ff00d6ecf93d3f9e1
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-07-28 15:31:37 +0100

    finally ready to start

--- solution.py
+++ solution.py
@@ -1,3 +1,10 @@
 x = open(0).read().strip().split("\n")
 
+LB = 7 if len(x) == 5 else 200000000000000
+UB = 27 if len(x) == 5 else 400000000000000
+
+hailstones = [(z[0].split(", "), z[1].split(", ")) for z in [y.split(" @ ") for y in x]]
+
 print(x)
+print(LB, UB)
+print(hailstones)
